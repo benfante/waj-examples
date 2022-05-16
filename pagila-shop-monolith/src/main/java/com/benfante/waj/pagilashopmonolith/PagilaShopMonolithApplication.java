3d6c4b25f68dc275e05abc8169a726cf877e4f74
Lucio Benfante <lucio@benfante.com>
2022-05-16 08:30:21 +0200

    Initial commit

---
+++ pagila-shop-monolith/src/main/java/com/benfante/waj/pagilashopmonolith/PagilaShopMonolithApplication.java
@@ -0,0 +1,13 @@
+package com.benfante.waj.pagilashopmonolith;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PagilaShopMonolithApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PagilaShopMonolithApplication.class, args);
+	}
+
+}
---
+++ pagila-shop-monolith/src/test/java/com/benfante/waj/pagilashopmonolith/PagilaShopMonolithApplicationTests.java
@@ -0,0 +1,13 @@
+package com.benfante.waj.pagilashopmonolith;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PagilaShopMonolithApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
